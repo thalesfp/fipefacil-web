4b782f0e7f70aed1e052b6b68f48c7e23f039e94
Thales Pereira <thalesfp@gmail.com>
2020-05-06 13:20:43 -0700

    Refactor reference for mobile

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,6 +7,7 @@ import Typography from "@material-ui/core/Typography";
 import IconButton from "@material-ui/core/IconButton";
 import { makeStyles } from "@material-ui/core/styles";
 import CircularProgress from "@material-ui/core/CircularProgress";
+import Hidden from "@material-ui/core/Hidden";
 import ArrowBack from "@material-ui/icons/ArrowBack";
 
 import currentReference from "../queries/currentReference";
@@ -50,9 +51,18 @@ function NavBar({ currentReference, backLink, hideBackButton }) {
             Fipe Fácil
           </Typography>
           {currentReference ? (
-            <Typography>
-              Referência - <b>{normalizeReferenceDate(currentReference)}</b>
-            </Typography>
+            <>
+              <Hidden smUp>
+                <Typography>
+                  {normalizeReferenceDate(currentReference)}
+                </Typography>
+              </Hidden>
+              <Hidden xsDown>
+                <Typography>
+                  Referência - <b>{normalizeReferenceDate(currentReference)}</b>
+                </Typography>
+              </Hidden>
+            </>
           ) : (
             <CircularProgress className={classes.loading} size={24} />
           )}
