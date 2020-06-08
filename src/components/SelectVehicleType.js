eba813e0eb33619a400b24a47bf64bb173c17093
Thales Pereira <thalesfp@gmail.com>
2020-06-08 08:22:57 -0700

    WIP: refactor

--- src/components/SelectVehicleType.js
+++ src/components/SelectVehicleType.js
@@ -1,18 +1,11 @@
 import React from "react";
-import { isMobile } from "react-device-detect";
 import FormControl from "@material-ui/core/FormControl";
 import InputLabel from "@material-ui/core/InputLabel";
-import MenuItem from "@material-ui/core/MenuItem";
 
 import Select from "./Select";
+import Option from "./Option";
 
 function SelectVehicleType({ vehicleTypeSelected, handleOnChange }) {
-  const options = [
-    { value: 1, label: "Carro" },
-    { value: 2, label: "Moto" },
-    { value: 3, label: "Caminhão" },
-  ];
-
   return (
     <div>
       <FormControl fullWidth={true}>
@@ -22,17 +15,9 @@ function SelectVehicleType({ vehicleTypeSelected, handleOnChange }) {
           value={vehicleTypeSelected}
           onChange={(event) => handleOnChange(parseInt(event.target.value, 10))}
         >
-          {options.map(({ value, label }) =>
-            isMobile ? (
-              <option key={label} value={value}>
-                {label}
-              </option>
-            ) : (
-              <MenuItem key={label} value={value}>
-                {label}
-              </MenuItem>
-            )
-          )}
+          <Option value={1}>Carro</Option>
+          <Option value={2}>Moto</Option>
+          <Option value={3}>Caminhão</Option>
         </Select>
       </FormControl>
     </div>
