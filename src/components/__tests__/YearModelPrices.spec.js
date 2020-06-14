7238b4d69584485ce6976903a7e0850f937e84c8
Thales Pereira <thalesfp@gmail.com>
2020-06-13 22:55:02 -0700

    Bug fix

--- src/components/__tests__/YearModelPrices.spec.js
+++ src/components/__tests__/YearModelPrices.spec.js
@@ -38,15 +38,15 @@ describe("YearModelPrices", () => {
 
       expect(screen.getByText("2018")).toBeDefined();
       expect(screen.getByText("Gasolina")).toBeDefined();
-      expect(screen.getByText("R$45,977.00")).toBeDefined();
+      expect(screen.getByText(/R\$\s?45,977.00/)).toBeDefined();
 
       expect(screen.getByText("2019")).toBeDefined();
       expect(screen.getByText("Álcool")).toBeDefined();
-      expect(screen.getByText("R$47,507.00")).toBeDefined();
+      expect(screen.getByText(/R\$\s?47,507.00/)).toBeDefined();
 
       expect(screen.getByText("Zero KM")).toBeDefined();
       expect(screen.getByText("Diesel")).toBeDefined();
-      expect(screen.getByText("R$50,421.00")).toBeDefined();
+      expect(screen.getByText(/R\$\s?50,421.00/)).toBeDefined();
     });
   });
 });
